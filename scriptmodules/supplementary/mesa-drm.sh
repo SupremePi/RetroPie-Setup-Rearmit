c1d8fb03089961e14bb839c6fe18af34ccca1931
Arnaldo Valente <gleam2003@msn.com>
2023-01-15 21:31:36 +0100

    modified:   scriptmodules/supplementary/mesa-drm.sh 	modified:   scriptmodules/supplementary/mesa3d.sh

--- scriptmodules/supplementary/mesa-drm.sh
+++ scriptmodules/supplementary/mesa-drm.sh
@@ -9,33 +9,42 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="mesa3d"
-rp_module_desc="mesa3d - userspace library for mesa3d for panfrost and lima"
+rp_module_id="mesa-drm"
+rp_module_desc="libdrm - userspace library for drm"
 rp_module_licence="MIT https://www.mesa3d.org/license.html"
-rp_module_repo="git https://gitlab.freedesktop.org/mesa/mesa.git mesa-22.3.3"
+rp_module_repo="git https://github.com/RetroPie/mesa-drm runcommand_debug"
 rp_module_section="depends"
-rp_module_flags="armbian"
+rp_module_flags=""
 
-function depends_mesa3d() {
-    local depends=(meson ninja-build libgbm-dev libdrm-dev libpciaccess-dev python3-mako libxml2-dev libzstd-dev pkg-config)
+function depends_mesa-drm() {
+    local depends=(meson ninja-build libgbm-dev libdrm-dev libpciaccess-dev)
 
     getDepends "${depends[@]}"
 }
 
-function sources_mesa3d() {
+function sources_mesa-drm() {
     gitPullOrClone
 }
 
-function build_mesa3d() {
-    rp_installModule "libdrm" "_autoupdate_"
+function build_mesa-drm() {
+    local params=()
 
-    meson builddir -Dprefix=/usr/local -Doptimization=3 --buildtype=release -Db_pie=false -Dstrip=false -Dgallium-omx=disabled -Dpower8=disabled -Dcpp_rtti=false -Ddri3=disabled -Dllvm=disabled -Dgallium-opencl=disabled -Dglx=disabled -Dgallium-xa=disabled -Dshared-glapi=enabled -Dgallium-drivers=panfrost,lima -Dgallium-extra-hud=true -Ddri-drivers= -Dvulkan-drivers= -Dosmesa=false -Dopengl=true -Dplatforms= -Dgbm=enabled -Degl=enabled -Dgles1=enabled -Dgles2=enabled -Dgallium-xvmc=disabled -Dvalgrind=disabled -Dlibunwind=disabled -Dgallium-vdpau=disabled -Dlmsensors=disabled -Dzstd=enabled -Dglvnd=false
+    # for RPI, disable all but VC4 driver to minimize startup delay
+    isPlatform "rpi" && params+=(-Dintel=false -Dradeon=false \
+                           -Damdgpu=false -Dexynos=false \
+                           -Dnouveau=false -Dvmwgfx=false \
+                           -Domap=false -Dfreedreno=false \
+                           -Dtegra=false -Detnaviv=false -Dvc4=true)
+    meson builddir --prefix="$md_inst" "${params[@]}"
     ninja -C builddir
 
-    md_ret_require="$md_build/builddir/src/gbm/libgbm.so.1.0.0"
+    md_ret_require="$md_build/builddir/tests/modetest/modetest"
 }
 
-function install_mesa3d() {
-    ninja -C builddir install
-    ldconfig
+function install_mesa-drm() {
+    md_ret_files=(
+        builddir/libkms/libkms.so*
+        builddir/tests/modetest/modetest
+    )
 }
+
