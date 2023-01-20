f22583302066da1a01c18e656f1f5a7ceea3b8a5
Arnaldo Valente <gleam2003@msn.com>
2023-01-20 17:19:16 +0100

    Working with signature key

--- scriptmodules/helpers.sh
+++ scriptmodules/helpers.sh
@@ -1684,7 +1684,12 @@ function adminRsync() {
 function signFile() {
     local file="$1"
     local cmd_out
-    cmd_out=$(gpg --default-key "$__gpg_signing_key" --detach-sign --armor --yes "$1" 2>&1)
+    [[ ! -v __gpg_passphrase_file ]] && __gpg_passphrase_file="/tmp/overlay/passphrase"
+    if [[ -f "$__gpg_passphrase_file" ]]; then
+        cmd_out=$(gpg --pinentry-mode loopback --passphrase-file="$__gpg_passphrase_file" --default-key "$__gpg_signing_key" --detach-sign --armor --yes "$1" 2>&1)
+    else
+        cmd_out=$(gpg --default-key "$__gpg_signing_key" --detach-sign --armor --yes "$1" 2>&1)
+    fi
     if [[ "$?" -ne 0 ]]; then
         md_ret_errors+=("Failed to sign $1\n\n$cmd_out")
         return 1
